5d8b00f4b7de229386636c2fac59d075c4390e60
satish <satishkonduru0225@gmail.com>
2023-11-22 18:28:05 +0530

    ngxUiLoader installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,27 @@ import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { SignupComponent } from './components/signup/signup.component';
 
+import { 
+  NgxUiLoaderModule,
+  NgxUiLoaderConfig, 
+  SPINNER,
+  PB_DIRECTION
+ } from 'ngx-ui-loader';
+
+
+const ngxUiLoaderConfig : NgxUiLoaderConfig = {
+   text: "Loading...",
+   textColor: "orange",
+   textPosition: "center-center",
+   pbColor: "orange",
+   bgsColor: "orange",
+  fgsColor:"orange",
+  fgsType: SPINNER.squareJellyBox,
+  fgsSize: 100,
+  pbDirection: PB_DIRECTION.leftToRight,
+  pbThickness: 5
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +45,8 @@ import { SignupComponent } from './components/signup/signup.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
